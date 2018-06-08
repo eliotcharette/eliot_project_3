27c8a50bc85a98df52b001af68f62b55cc7f1de8
Guest User <Guest@epicodus-4.local>
2018-06-08 09:59:05 -0700

    Add for loop to scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,41 @@
 var beepboop = function (input) {
 
+  if (input < 0){
+    return "Please enter a positive number.";
+  }
+
+  var number1 = [];
+
+  for (var i = 0; i <= input; i++){
+    number1.push(i);
+  }
+  if (input % 3 = 0)
+
+  /*if (input = 3){
+    number1.push('Beep!')
+  }
+  if (input = 4){
+    number1.push('Boop!')
+  }
+  for (var i = 0; i <= input; i++){
+    if(input % 3 = 0){
+      number1.push(i)
+    }
+  }*/
+
+var total = number1;
+
+
+
+
+
+
+
 $(".result").text(total)
 }
 
 
-dfß
+
 
 
 $(function() {
