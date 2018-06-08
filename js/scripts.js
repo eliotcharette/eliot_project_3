dd85cae8ab32c13f8e25d395bfe6b9710b22cb2d
Guest User <Guest@epicodus-4.local>
2018-06-08 10:38:03 -0700

    add for loop to replace variables of 3 with specified string im sorry dave..

--- js/scripts.js
+++ js/scripts.js
@@ -7,17 +7,18 @@ var beepboop = function (input) {
   var number2 = [];
 
 
-  for (var i = 0; i <= input; i++){
+  for (var i = 1; i <= input; i++){
     number1.push(i);
   };
- for (var i = 0; i < number1.length; i++){
-   if(number1[i] % 3 !== 0){
-     number2.push(i);
+ for (var i = 1; i < number1.length; i++){
+   if(number1[i] % 3 === 0){
+     number1[i] = ("I'm sorry, Dave I'm afraid I can't do that.");
    }
  }
- for (var i = 0; i < number2.length; i++){
-   if(number2[i] )
- }
+ 
+ //for (var i = 0; i < number2.length; i++){
+//   if(number2[i] )
+ //}
 
 
   /*if number1 % 3 = 0 {
@@ -42,7 +43,7 @@ for (var i = 0; i <= input; i++){
     }
   }*/
 
-var total = number2;
+var total = number1;
 
 
 
